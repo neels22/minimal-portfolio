f331a6245e7708fe8348df610e0bd68511f0b5c8
neels22 <indraneelsarode@gmail.com>
2025-04-05 00:32:54 -0700

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output:"export",
   images: {
-    output:"export",
     unoptimized: true,
     domains: [
       "source.unsplash.com",
